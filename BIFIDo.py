a6c4cd3d26dd58c1c6536a6146a39721b853a783
seangibbons <monsieursean@gmail.com>
2016-01-29 17:26:12 -0500

    update

--- BIFIDo.py
+++ BIFIDo.py
@@ -17,16 +17,17 @@ from pykalman import KalmanFilter
 
 
 ###imput arguments
+parser = argparse.ArgumentParser()
 parser.add_argument('-i', help='input file (rows = samples, columns = OTUs)', required=True)
-parser.add_argument('-t', help='input data type', default='norm', choices=['counts', 'norm', 'log'])
+parser.add_argument('-t', help='input data type', default='counts', choices=['counts', 'norm', 'log'])
 parser.add_argument('-a', help='min coefficient', default=.1, type=float)
 parser.add_argument('-b', help='max coefficient', default=10, type=float)
 parser.add_argument('-d', help='field delimiter', default='\t')
 parser.add_argument('-o', help='output prefix ', default='out')
+args = parser.parse_args()
 
 # read data - make sure sample names are converted to Pandas-interpretable datatime format (e.g. YYYY-MM-DD)
-x = pd.read_table(args.i, sep=args.d, header=0, index_col=0, infer_datetime_format=True)
-
+x = pd.DataFrame.from_csv(args.i, sep=args.d, header=0, index_col=0, infer_datetime_format=True)
 # uncomment transpose if OTUs are rows and samples are columns
 #x = x.transpose()
 
@@ -42,14 +43,18 @@ if nrowx == 0 or ncolx == 0:
 if x.min().min() < 0 or x.max().max() > 1:
     quit('error: input file format (check data type)')
 
+print "Data successfully loaded!"
+
 # fill missing dates with 'nan'
 x_nans = x.resample('D')
 
+print "Gaps in time series filled with NaNs!"
+
 # use numpy mask for nans - required for pykalman
-x_nans_masked = ma.masked_invalid(np.array(x_nans))
+x_nans_masked = np.ma.masked_invalid(np.array(x_nans))
 
 # initialize dataframe for kalman-smoothed data
-x_int_ks = pd.DataFrame(np.zeros([x_nans_masked.shape[0],x_nans_masked.shape[1]),index=x_nans_masked.index,columns=x_nans_masked.columns)
+x_int_ks = pd.DataFrame(np.zeros((x_nans_masked.shape[0],x_nans_masked.shape[1])),index=x_nans.index,columns=x_nans.columns)
 
 # initialize kalman filter function for 1-D time series
 kf = KalmanFilter(n_dim_obs=1)
@@ -60,8 +65,14 @@ for i in range(x_nans_masked.shape[1]):
     for x in range(x_nans_masked.shape[0]):
         x_int_ks.iloc[x,i] = np.array(filtered_state_means).flatten()[x]
 
+print "Kalman smoothing and interpolation successful!"
+
+
 #log-transform kalman smoothed data
 x_int_ks_log = x_int_ks.apply(np.log)
+
+print "Log transform successful!"
+
 
 #initialize dataframe for first-differenced data
 x_int_ks_log_delta = x_int_ks_log.iloc[:-1,:]
@@ -74,18 +85,20 @@ for i in range(x_int_ks_log.shape[1]):
         log_delta_list.append(log_delta)
     x_int_ks_log_delta[x_int_ks_log.columns[i]] = np.transpose(log_delta_list)
 
-nan_file_handle = '%d_nan.txt' % args.t
-nan_masked_file_handle = '%d_nan_masked.txt' % args.t
-ks_file_handle = '%d_ks.txt' % args.t
-ks_log_file_handle = '%d_ks_log.txt' % args.t
-ks_log_delta_file_handle = '%d_ks_log_delta.txt' % args.t
+print "First differences successfully calculated!"
+
+
+nan_file_handle = '%s_nan.txt' % args.o
+ks_file_handle = '%s_ks.txt' % args.o
+ks_log_file_handle = '%s_ks_log.txt' % args.o
+ks_log_delta_file_handle = '%s_ks_log_delta.txt' % args.o 
 
 
 x_nans.to_csv(nan_file_handle, sep='\t')
-x_nans_masked.to_csv(nan_masked_file_handle, sep='\t')
 x_int_ks.to_csv(ks_file_handle, sep='\t')
 x_int_ks_log.to_csv(ks_log_file_handle, sep='\t')
 x_int_ks_log_delta.to_csv(ks_log_delta_file_handle, sep='\t')
 
 
+print "Data written to file!"
 
